b0770522551073b9bc254efaa0fd4e69a5796784
umama khan <160400357+umamak064@users.noreply.github.com>
2024-07-04 14:48:07 -0700

    Add files via upload

---
+++ Q4_quote.js
@@ -0,0 +1,3 @@
+var quote = "If you can dream it, you can do it.";
+var author = "Walt Disney";
+console.log("".concat(author, " once said, \"").concat(quote, "\"."));
---
+++ Q4_quote.ts
@@ -0,0 +1,3 @@
+let quote: string = "If you can dream it, you can do it.";
+let author: string = "Walt Disney";
+console.log(`${author} once said, "${quote}".`);
