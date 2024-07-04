8fbe21d4b37b7de9a881cb20629978f5c04ed727
umama khan <160400357+umamak064@users.noreply.github.com>
2024-07-04 14:48:29 -0700

    Add files via upload

---
+++ Q5_quote.js
@@ -0,0 +1,4 @@
+var famousPerson = "Walt Disney";
+var quote = "If you can dream it, you can do it.";
+var message = "".concat(famousPerson, " once said, \"").concat(quote, "\".");
+console.log(message);
---
+++ Q5_quote.ts
@@ -0,0 +1,5 @@
+let famousPerson: string = "Walt Disney";
+let quote: string = "If you can dream it, you can do it.";
+let message: string = `${famousPerson} once said, "${quote}".`;
+
+console.log(message);
